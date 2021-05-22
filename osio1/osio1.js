4446421d9dc46dfbbafa97ffbc3be7a4f3b84ac0
Jere Länsipii <ojela028@edu.xamk.fi>
2021-05-22 18:16:34 +0300

    herokuapp v1

--- osio1/osio1.js
+++ osio1/osio1.js
@@ -5,6 +5,59 @@
  * 
  */
 
+const myQuestions = [
+  {
+    question: "Mitä tarkoittaa digitaalinen jalanjälki?",
+    answers: {
+      a: "Kaikkia tiedostoja, joita luvatta jaetaan netissä.",
+      b: "Rikolliseksi epäilyttävän tai haitallisen toiminnan ilmoittamista viranomaisille. ",
+      c: "Kaikkea bittimerkintöjä eli dataa, jotka jätät taaksepäin käyttäessäsi internettiä.",
+      d: "Mikä tahansa haittaohjelma, joka vaikuttaa tietokoneen käyttöön."
+    },
+    correctAnswer: "c"
+  },
+  {
+    question: "Netissä ei kannata jakaa julkisesti ihan mitä huvittaa. Mitä näistä vaihtoehdoista on kannattavaa jakaa?",
+    answers: {
+      a: "Kuva sinusta kesällä vanhempiesi mökillä pelkillä alusvaatteilla.",
+      b: "Henkilökohtaisia asioita, jotka saattavat satuttaa sinua.",
+      c: "Blogikirjoitus ja kuvia lemmikistäsi.",
+      d: "Bilekutsu, jossa mainitset asuinpaikkasi."
+    },
+    correctAnswer: "c"
+  },
+  {
+    question: "Mitä seuraavista tiedoista meistä kerätään netissä?",
+    answers: {
+      a: "Tietokoneesi akun varaustilaa.",
+      b: "Tietoa opiskeluistasi",
+      c: "Missä vietät eniten aikaa.",
+      d: "Kaikki edellä mainitut."
+    },
+    correctAnswer: "d"
+  },
+  {
+    question: "Mikä seuraavista väittämistä EI pidä paikkansa?",
+    answers: {
+      a: "Kerätyn datan avulla voidaan ennustaa ihmisen käytöstä.",
+      b: "Datasta muodostuvaa kokonaiskuvaa ihmisestä nimitetään digitaaliseksi profiiliksi.",
+      c: "Kaikki sähköisessä muodossa esitettävä tieto voidaan jakaa materiaalina verkon yli.",
+      d: "Kerätyn datan avulla voidaan kohdentaa mainontaa."
+    },
+    correctAnswer: "b"
+  },
+  {
+    question: "Mikä seuraavista väittämistä pitää paikkansa?",
+    answers: {
+      a: "Jos et ole julkisuuden henkilö etkä rikollinen, niin tietojen luovuttamisesta ei ole haittaa.",
+      b: "Kanta-asiakasohjelmista ja korttimaksuista ei jää jälkeä nettiin.",
+      c: "Tiedon säilyttäminen on halvempaa kuin tiedon tuhoaminen.",
+      d: "Netti voi kerätä tietoja sinusta, mutta ei sinun perhesuhteista tai ystäväpiiristä."
+    },
+    correctAnswer: "c"
+  }
+];
+
 (function(){
     function buildQuiz(){
       // variable to store the HTML output
@@ -64,6 +117,7 @@
 
       // Funktiot sen jälkeen, kun vastaukset on tarkistettu
       if (numCorrect > 3) {
+        /*
         myQuestions.forEach( (currentQuestion, questionNumber) => { 
 
           const answerContainer = answerContainers[questionNumber];
@@ -75,70 +129,22 @@
           } else {
             answerContainers[questionNumber].style.color = 'red';
           }
-        });
+        }); */
         testiSuoritettu();
       } else {
         testiHylatty();
       }
   
       // show number of correct answers out of total
+      /*
       resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+      */
     }
   
     const quizContainer = document.getElementById('quiz');
     const resultsContainer = document.getElementById('results');
     const submitButton = document.getElementById('submit');
-    const myQuestions = [
-        {
-          question: "Mikä näistä sivustoista ei ole huijaussivusto?",
-          answers: {
-            a: "www.youtube.fi",
-            b: "www72.youtube.com",
-            c: "www.y0utube.com",
-            d: "www.youtube.org"
-          },
-          correctAnswer: "a"
-        },
-        {
-          question: "Haluat katsoa yhden suosikki elokuvistasi. Mitä kautta aiot elokuvan hankkia?",
-          answers: {
-            a: "Katsomalla elokuvan jostakin maksullisesta suoratoisto palvelusta.",
-            b: "Lataamalla elokuvan torrent sivustolta tietokoneellesi.",
-            c: "Katsomalla elokuvan nettisivulta, jonne on ladattu paljon elokuvia ilmaiseksi",
-            d: "Lataamalla elokuvan ilmaiseksi internetistä."
-          },
-          correctAnswer: "a"
-        },
-        {
-          question: "Tuntematon henkilö lähettää sinulle Instagramissa yksityisviestin. Mitä näistä viesteistä EI lasketa uhkailuksi?",
-          answers: {
-            a: '"Lähetä rahaa mun tilille tai ilmiannan sun profiilin asiattomasta materiaalista!"',
-            b: '"Lähetä mulle alastonkuvia itsestäsi tai kerron sun mutsille noista kuvista, jossa poltat röökiä."',
-            c: '"Anna mulle toi skini tos pelis tai mä tuun sun kotiovelle. Mä tiiän missä sä asut."',
-            d: '"Lähetä mulle ystäväpyyntö, mun mielestä sulla on upea profiili."'
-          },
-          correctAnswer: "d"
-        },
-        {
-          question: "Internetissä ei saa julkaista tekijänoikeuslain vastaisia materiaaleja. Mikä näistä vaihtoehdoista NOUDATTAA tekijänoikeuslakia?",
-          answers: {
-            a: "Kopioit runokirjasta runon ja postaat sen omalla nimelläsi Facebook-seinällesi.",
-            b: "Lataat lempi bändistäsi puhuvan vlogin YouTube kanavallesi.",
-            c: "Lataat lempi bändisi kaupallisesti myydyn kappaleen YouTube kanavallesi.",
-            d: "Jaat torrent-ohjelmalla lempi elokuvasi muiden katsottavaksi."
-          },
-          correctAnswer: "b"
-        },
-        {
-          question: "Sähköpostiin saattaa välillä ilmestyä niin sanottua roskapostia. Mikä seuraavista vaihtoehdoista EI ole roskapostia?",
-          answers: {
-            a: '"Hei, oletko kuullut tosta Big Money –verkkosivustosta? Meille tulee niiltä mailia kokoajan!"',
-            b: '"Myynnissä uusi rikkapölynimuri nyt vain 29,99€. Saatavilla vain rajoitetun ajan!"',
-            c: '"Olet voittanut lotossa 3 miljoona euroa! Lunasta voittasi vierailemalla sivulle https://bigmoneyboom.com/prizes"'
-          },
-          correctAnswer: "a"
-        }
-      ];
+
   
     // Kick things off
     buildQuiz();
@@ -147,3 +153,87 @@
     submitButton.addEventListener('click', showResults);
 
   })();
+
+/*
+ * By Jere Länsipii
+ * 2021
+ * 
+ */
+
+const submitButton = document.getElementById('submit');
+const ilmoitusNappi = document.getElementById("ilmoitusNappi");
+const suljeNappi = document.getElementById("suljeNappi");
+const modal = document.getElementById("ilmoitusIkkuna");
+
+function onkoSuoritettu() {
+  // katsotaan onko osio jo suoritettu
+  laskuri();
+  var suoritus1 = localStorage.getItem("suoritus1");
+  if (suoritus1 == 1) {
+    hyvaksyttyIkkuna.style.display = "block";
+    quiz.style.display = "none";
+    ilmoitusNappi.style.display = "none";   
+  }
+  
+}
+
+function testiSuoritettu() {
+    modal.style.display = "none";
+    hyvaksyttyIkkuna.style.display = "block";
+    quiz.style.display = "none";
+    ilmoitusNappi.hidden = true;
+    // LocalStorage
+    if (typeof(Storage) !== "undefined") {
+    // Store
+    localStorage.setItem("suoritus1", 1);
+    } else {
+    console.log("Sorry, your browser does not support Web Storage...");
+    }
+};
+
+function testiHylatty() {
+    modal.style.display = "none";
+    hylattyIkkuna.style.display = "block";
+    // disabloi nappulat ja kysymysalue ja yritä myöhemmin uudelleen
+    ilmoitusNappi.hidden = true;
+    quiz.style.display = "none";
+    // asetetaan "countdown"
+    const currentTime = new Date().getTime();
+    localStorage.setItem("hylatty1", currentTime);
+    
+};
+
+ilmoitusNappi.onclick = function() {
+  modal.style.display = "block";
+}
+suljeNappi.onclick = function() {
+    modal.style.display = "none";
+}
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
+function laskuri() {
+  const expirationDuration = 1000 * 60 * 60 * 1; // 1 hour
+  const prevAccepted = localStorage.getItem("hylatty1");
+  const currentTime = new Date().getTime();
+  const notAccepted = prevAccepted == undefined;
+  const prevAcceptedExpired = prevAccepted != undefined && currentTime - prevAccepted > expirationDuration;
+    if (notAccepted || prevAcceptedExpired) {
+      countdown.style.display = "none";
+      quiz.style.display = "block";
+      ilmoitusNappi.hidden = false;
+    } else {
+      quiz.style.display = "none";
+      ilmoitusNappi.hidden = true;
+      countdown.style.display = "block";
+      hylattyIkkuna.style.display = "block";
+      let htmlAika = new Date(parseInt(localStorage.getItem('hylatty1')));
+      h = (htmlAika.getHours()<10?'0':'') + (htmlAika.getHours() + 1),
+      m = (htmlAika.getMinutes()<10?'0':'') + htmlAika.getMinutes();
+      document.getElementById("countdown").innerHTML = "Yritä uudelleen klo " + h + " : " + m;
+    }
+}
